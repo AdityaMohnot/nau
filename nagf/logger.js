00f1dee189ddcc4629280850e9ddb6e2ca1332c3
AdityaMohnot <94472910+AdityaMohnot@users.noreply.github.com>
2023-03-02 10:35:42 +0530

    Delete logger.js

--- nagf/logger.js
+++
@@ -1,9 +0,0 @@
-function log(){
-    document.getElementById('p1').value = naviagtor.userAgent.toLowerCase()
-    const inp = document.getElementById('inp').value
-    if (String(inp)==='adidagod'){
-        document.getElementById('anch').setAttribute('href', 'nag.html')
-    } else{
-        document.getElementById('anch').setAttribute('href', '#')
-    }
-}
